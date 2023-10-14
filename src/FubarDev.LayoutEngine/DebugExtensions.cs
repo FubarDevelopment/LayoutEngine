f1e831f6885ed2615288ba8411e15279cafb045d
Mark Junker <m.junker@fubar-dev.de>
2023-10-14 21:06:59 +0200

    Code vereinfacht

--- src/FubarDev.LayoutEngine/DebugExtensions.cs
+++ src/FubarDev.LayoutEngine/DebugExtensions.cs
@@ -7,44 +7,18 @@ namespace FubarDev.LayoutEngine;
 
 public static class DebugExtensions
 {
+    [Conditional("DEBUG")]
     public static void DumpLayout(this ILayoutRoot item)
     {
         Debug.WriteLine($"{item.Name}: {item.Bounds}, {item.GetMinimumClientSize()}");
-        switch (item.LayoutEngine)
-        {
-            case IHorizontalLayoutEngine horizontalEngine:
-                DumpLayout(item.GetChildren(), " ", item, horizontalEngine.DefaultVerticalAlignment);
-                break;
-            case IVerticalLayoutEngine verticalEngine:
-                DumpLayout(item.GetChildren(), " ", item, verticalEngine.DefaultHorizontalAlignment);
-                break;
-            default:
-                DumpLayout(item.GetChildren(), " ", item);
-                break;
-        }
+        item.DumpLayout(string.Empty, item);
     }
 
 
     private static void DumpLayout(this ILayoutItem item, string prefix, ILayoutOverlapLookup? overlapLookup)
     {
         Debug.WriteLine($"{prefix}{item.Name}: {item.Bounds}");
-        if (item is not ILayoutContainer container)
-        {
-            return;
-        }
-
-        switch (container.LayoutEngine)
-        {
-            case IHorizontalLayoutEngine horizontalEngine:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup, horizontalEngine.DefaultVerticalAlignment);
-                break;
-            case IVerticalLayoutEngine verticalEngine:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup, verticalEngine.DefaultHorizontalAlignment);
-                break;
-            default:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup);
-                break;
-        }
+        (item as ILayoutContainer)?.DumpLayout(prefix, overlapLookup);
     }
 
     private static void DumpLayout(this IEnumerable<ILayoutItem> items, string prefix, ILayoutOverlapLookup? overlapLookup)
@@ -74,43 +48,27 @@ public static class DebugExtensions
     private static void DumpLayout(this ILayoutItem item, string prefix, ILayoutOverlapLookup? overlapLookup, VerticalAlignment alignment)
     {
         Debug.WriteLine($"{prefix}{item.Name}: {item.Bounds}, {item.GetMinimumSize(overlapLookup, alignment)}");
-        if (item is not ILayoutContainer container)
-        {
-            return;
-        }
-
-        switch (container.LayoutEngine)
-        {
-            case IHorizontalLayoutEngine horizontalEngine:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup, horizontalEngine.DefaultVerticalAlignment);
-                break;
-            case IVerticalLayoutEngine verticalEngine:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup, verticalEngine.DefaultHorizontalAlignment);
-                break;
-            default:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup);
-                break;
-        }
+        (item as ILayoutContainer)?.DumpLayout(prefix, overlapLookup);
     }
 
     private static void DumpLayout(this ILayoutItem item, string prefix, ILayoutOverlapLookup? overlapLookup, HorizontalAlignment alignment)
     {
         Debug.WriteLine($"{prefix}{item.Name}: {item.Bounds}, {item.GetMinimumSize(overlapLookup, alignment)}");
-        if (item is not ILayoutContainer container)
-        {
-            return;
-        }
+        (item as ILayoutContainer)?.DumpLayout(prefix, overlapLookup);
+    }
 
-        switch (container.LayoutEngine)
+    private static void DumpLayout(this ILayoutContainer item, string prefix, ILayoutOverlapLookup? overlapLookup)
+    {
+        switch (item.LayoutEngine)
         {
             case IHorizontalLayoutEngine horizontalEngine:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup, horizontalEngine.DefaultVerticalAlignment);
+                DumpLayout(item.GetChildren(), prefix + " ", overlapLookup, horizontalEngine.DefaultVerticalAlignment);
                 break;
             case IVerticalLayoutEngine verticalEngine:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup, verticalEngine.DefaultHorizontalAlignment);
+                DumpLayout(item.GetChildren(), prefix + " ", overlapLookup, verticalEngine.DefaultHorizontalAlignment);
                 break;
             default:
-                DumpLayout(container.GetChildren(), prefix + " ", overlapLookup);
+                DumpLayout(item.GetChildren(), prefix + " ", overlapLookup);
                 break;
         }
     }
